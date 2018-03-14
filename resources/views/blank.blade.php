4fe5697a9ea6f0fd6f631f9fcf2329c43b89dffb
J0Sepff <xmagistrx@gmail.com>
2018-03-14 16:04:56 +0300

    new

---
+++ resources/views/blank.blade.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: J0Seph
+ * Date: 14/3/18
+ * Time: 3:57 PM
+ */?>
+
+<!DOCTYPE html>
+<html lang="ru" >
+<head>
+    <meta charset="utf-8">
+    <title>Анкета</title>
+    <link rel="stylesheet" href="">
+</head>
+<body>
+
+</body>
+</html>
